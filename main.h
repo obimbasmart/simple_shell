90faf1c64c7945157f502084cfaf3c3669757c5a
obimbasmart <obimbasmartchukwunenye@gmail.com>
2022-11-22 12:29:18 +0000

    initial commit

---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef main_h
+#define main_h
+
+#include <stdlib.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+
+char **split_string(char *, char *);
+void remove_new_line(char **, size_t);
+
+#endif /* end of main_h */
---
+++ simple_super_shell.c
@@ -0,0 +1,48 @@
+#include "main.h"
+
+int main(__attribute__((unused))int argc, 
+		__attribute__((unused)) char **argv, char **env)
+{
+	int status;
+	char *lineptr; 
+	char *new_argv[2];
+	size_t lineLen;
+	size_t nread;
+	extern int errno;
+
+	lineptr = NULL;
+	lineLen = 0;
+	new_argv[0] = NULL;
+
+	printf("#mySimpleShell$: ");
+
+	while ((nread = getline(&lineptr, &lineLen, stdin)))
+	{
+		/* the getline() function returns the entire line from
+		 * a stream including the new line character '\n' if one was found
+		 * this new line character when included in a file pathname
+		 * leads to invalid pathname - hence the new line char
+		 * would be removed before passing the pathname to execve() function
+		 */
+		remove_new_line(&lineptr, nread);
+
+		new_argv[0] = lineptr;
+		new_argv[1] = NULL;
+
+		status = 0;
+
+		if (fork() == 0)
+		{
+			if (execve(new_argv[0], new_argv, env) == -1)
+				perror(new_argv[0]);
+
+			exit(0);
+		}
+
+		wait(&status);
+		printf("#mySimpleShell$: ");
+	}
+	fflush(stdout);	
+	return (0);
+}
+
